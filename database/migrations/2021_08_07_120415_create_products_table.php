af046e0f013e7b0e1b75da7b239fb62d005d8525
PoojaBora09 <pooja@privatedeal.com>
2021-08-08 14:46:51 +0530

    chnages

--- database/migrations/2021_08_07_120415_create_products_table.php
+++ database/migrations/2021_08_07_120415_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->integer('category_id');
+            $table->string('category_id');
             $table->string('SKU');
             $table->double('price',10,2);            
             $table->timestamps();
